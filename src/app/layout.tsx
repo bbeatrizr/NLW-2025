458b8d8e3c4ed9b63519dcea8c89318e0ed22398
bbeatrizr <beatriz.ramalho010@gmail.com>
2025-02-20 13:38:26 -0300

    docs: ✏️ organization components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,34 @@
-import "./global.css";
-import type { Metadata } from "next";
-import { Montserrat, Oxanium } from "next/font/google";
+import './global.css'
+import type { Metadata } from 'next'
+import { Montserrat, Oxanium } from 'next/font/google'
 
 export const metadata: Metadata = {
-  title: "devstage",
-};
+  title: 'devstage',
+}
 
 const oxanium = Oxanium({
-  weight: ["500", "600"],
-  subsets: ["latin"],
-  variable: "--font-oxanium",
-});
+  weight: ['500', '600'],
+  subsets: ['latin'],
+  variable: '--font-oxanium',
+})
 const montserrat = Montserrat({
-  weight: ["400", "600"],
-  subsets: ["latin"],
-  variable: "--font-montserrat",
-});
+  weight: ['400', '600'],
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+})
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en" className={`${oxanium.variable} ${montserrat.variable}`}>
       <body className="bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat md:bg-right-top bg-top">
-        {children}
+        <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
+          {children}
+        </main>
       </body>
     </html>
-  );
+  )
 }
